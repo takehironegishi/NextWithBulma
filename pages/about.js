24c8d846a87ff587e27be5eb86d3d972d2b6a578
takehironegishi <takehiro.n61@gmail.com>
2021-09-20 23:42:43 +0900

    feat: Layout.js

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import { Layout } from "../components/Layout"
+
+const About = () => {
+  return (
+    <Layout>
+      <h1>I am about page</h1>
+    </Layout>
+  )
+}
+
+export default About
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,17 @@
-import { Footer } from "../components/Footer"
-import { Navbar } from "../components/Navbar"
-import { Newsletter } from "../components/Newsletter"
-import { ResourceHighlight } from "../components/ResourceHighlight"
-import { ResourceList } from "../components/ResourceList"
+import { Footer } from "components/Footer"
+import { Layout } from "components/Layout"
+import { Newsletter } from "components/Newsletter"
+import { ResourceHighlight } from "components/ResourceHighlight"
+import { ResourceList } from "components/ResourceList"
 
 const Home = () => {
   return (
-    <>
-      <Navbar />
+    <Layout>
       <ResourceHighlight />
       <Newsletter />
       <ResourceList />
       <Footer />
-    </>
+    </Layout>
   )
 }
 

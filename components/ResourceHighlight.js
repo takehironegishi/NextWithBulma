41d2706c6951193a7dd6dbf2f9fd3dcae33f4b0e
takehironegishi <takehiro.n61@gmail.com>
2021-09-21 00:20:49 +0900

    feat: implement temporary fetching data

--- components/ResourceHighlight.js
+++ components/ResourceHighlight.js
@@ -1,39 +1,21 @@
-export const ResourceHighlight = () => {
-	return (
-		<section className="hero ">
-			<div className="hero-body">
-				<div className="container">
-					<section className="section">
+export const ResourceHighlight = ({ resources }) => (
+	<section className="hero ">
+		<div className="hero-body">
+			<div className="container">
+				{ resources.map(resource => (
+					<section className="section" key={resource.id}>
 						<div className="columns">
 							<div className="column is-8 is-offset-2">
 								<div className="content is-medium">
-									<h2 className="subtitle is-4">December 25, 2018</h2>
-									<h1 className="title">Getting Started</h1>
-									<p>This is a starter template for creating a beautiful, customizable blog with minimal
-										effort. You’ll only have to change a few settings and you’re ready to go. As with all Jigsaw sites,
-										configuration settings can be found in config</p>
+									<h2 className="subtitle is-4">{resource.createdAt}</h2>
+									<h1 className="title">{resource.title}</h1>
+									<p>{resource.description}</p>
 								</div>
 							</div>
 						</div>
 					</section>
-
-					<div className="is-divider"></div>
-
-					<section className="section">
-						<div className="columns">
-							<div className="column is-8 is-offset-2">
-								<div className="content is-medium">
-									<h2 className="subtitle is-4">December 25, 2018</h2>
-									<h1 className="title">Getting Started</h1>
-									<p>This is a starter template for creating a beautiful, customizable blog with minimal
-										effort. You’ll only have to change a few settings and you’re ready to go. As with all Jigsaw sites,
-										configuration settings can be found in config</p>
-								</div>
-							</div>
-						</div>
-					</section>
-				</div>
+				)) }
 			</div>
-		</section>
-	)
-}
+		</div>
+	</section>
+)
--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,14 @@ import { Layout } from "components/Layout"
 import { Newsletter } from "components/Newsletter"
 import { ResourceHighlight } from "components/ResourceHighlight"
 import { ResourceList } from "components/ResourceList"
+import { resources } from "api/data"
 
 const Home = () => {
   return (
     <Layout>
-      <ResourceHighlight />
+      <ResourceHighlight resources={resources.slice(0, 2)} />
       <Newsletter />
-      <ResourceList />
+      <ResourceList resources={resources.slice(2)} />
       <Footer />
     </Layout>
   )

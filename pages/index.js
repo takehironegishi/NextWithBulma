0aa19ef2ed23988646a22876281e11777c4ccae1
takehironegishi <takehiro.n61@gmail.com>
2021-09-22 01:04:45 +0900

    feat: implement getServerSideProps

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,8 @@ import { Layout } from "components/Layout"
 import { Newsletter } from "components/Newsletter"
 import { ResourceHighlight } from "components/ResourceHighlight"
 import { ResourceList } from "components/ResourceList"
-import { resources } from "api/data"
 
-const Home = () => {
+const Home = ({ resources }) => {
   return (
     <Layout>
       <ResourceHighlight resources={resources.slice(0, 2)} />
@@ -14,6 +13,17 @@ const Home = () => {
       <Footer />
     </Layout>
   )
+}
+
+export const getServerSideProps = async () => {
+  const resData = await fetch("http://localhost:3000/api/resources");
+  const data = await resData.json();
+
+  return {
+    props: {
+      resources: data
+    }
+  }
 }
 
 export default Home

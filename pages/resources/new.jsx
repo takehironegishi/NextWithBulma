1d414d691fdddae47f12612d7cbe5199030b6a1a
takehironegishi <takehiro.n61@gmail.com>
2021-09-23 15:59:21 +0900

    feat: implement form functionality

--- pages/resources/new.jsx
+++ pages/resources/new.jsx
@@ -1,6 +1,28 @@
 import { Layout } from "components/Layout"
+import { useState } from "react";
+
+const DEFAULT_DATA = {
+  title: "",
+  description: "",
+  link: "",
+  priority: "2",
+  timeToFinish: 60
+}
 
 const ResourceCreate = () => {
+  const [form, setForm] = useState(DEFAULT_DATA);
+
+  const submitForm = () => {
+    alert(JSON.stringify(form))
+  }
+
+  const resetForm = () => setForm(DEFAULT_DATA)
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  }
+
   return (
     <Layout>
       <div className="container">
@@ -12,21 +34,21 @@ const ResourceCreate = () => {
                 <div className="field">
                   <label className="label">Title</label>
                   <div className="control">
-                    <input className="input" type="text" placeholder="Learn Next JS and Sanity IO" />
+                    <input value={form.title} onChange={handleChange} name="title" className="input" type="text" placeholder="Learn Next JS and Sanity IO" />
                   </div>
                 </div>
 
                 <div className="field">
                   <label className="label">Description</label>
                   <div className="control">
-                    <textarea className="textarea" placeholder="Learn these technologies because they are very popular and enable better SEO"></textarea>
+                    <textarea value={form.description} onChange={handleChange} name="description" className="textarea" placeholder="Learn these technologies because they are very popular and enable better SEO"></textarea>
                   </div>
                 </div>
 
                 <div className="field">
                   <label className="label">Link</label>
                   <div className="control">
-                    <input className="input" type="text" placeholder="https://academy.eincode.com" />
+                    <input value={form.link} onChange={handleChange} name="link" className="input" type="text" placeholder="https://example.com" />
                   </div>
                 </div>
 
@@ -34,7 +56,7 @@ const ResourceCreate = () => {
                   <label className="label">Priority</label>
                   <div className="control">
                     <div className="select">
-                      <select>
+                      <select value={form.priority} onChange={handleChange}  name="priority">
                         <option>1</option>
                         <option>2</option>
                         <option>3</option>
@@ -46,16 +68,17 @@ const ResourceCreate = () => {
                 <div className="field">
                   <label className="label">Time to finish</label>
                   <div className="control">
-                    <input className="input" type="number" placeholder="60 (time is in minutes)" />
+                    <input value={form.timeToFinish} onChange={handleChange} name="timeToFinish" className="input" type="number" placeholder="60" />
                   </div>
+                  <p className="help">Time is in minutes</p>
                 </div>
 
                 <div className="field is-grouped">
                   <div className="control">
-                    <button className="button is-link">Submit</button>
+                    <button type="button" onClick={submitForm} className="button is-link">Submit</button>
                   </div>
                   <div className="control">
-                    <button className="button is-link is-light">Cancel</button>
+                    <button type="button" onClick={resetForm} className="button is-link is-light">Reset Form</button>
                   </div>
                 </div>
               </form>

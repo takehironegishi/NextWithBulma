4c876210ec0fdcb696a0ce74209c4e8bffbc5993
takehironegishi <takehiro.n61@gmail.com>
2021-09-25 00:11:59 +0900

    feat: validate

--- pages/api/resources.js
+++ pages/api/resources.js
@@ -8,6 +8,10 @@ export default async function resources(req, res) {
   
   if (req.method === "POST") {
     console.log(req.body);
+    const { title, description, link, timeToFinish, priority } = req.body;
+    if (!(title && description && link && timeToFinish && priority)) {
+      return res.status(422).send("Data are missing!");
+    }
     return res.send("Data has beenn received!")
   }
 }

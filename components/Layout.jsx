fdb42ffa0377935fc527fc41ee422b50bc71fede
takehironegishi <takehiro.n61@gmail.com>
2021-09-26 23:12:42 +0900

    feat: active resource component

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,9 +1,11 @@
 import { Navbar } from "./Navbar";
+import { ActiveResource } from "components/ActiveResource"
 
 
 export const Layout = ({ children }) => (
   <>
     <Navbar />
+    <ActiveResource />
     { children }
   </>
 )
